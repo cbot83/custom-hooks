6360e22325948bf30e1f4885b8c954aae7a7dcd8
Craig Rice <craigricetong@gmail.com>
2020-02-23 11:03:35 -0800

    added infite scroll hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { useInfiniteScroll } from "./useInfiniteScroll";
 
 const App = () => {
+  // Assign the value returned by the custom hook to a variable
+  let infiniteScroll = useInfiniteScroll();
+  //Declaring the state
+  const [tableContent, setTableConent] = useState([]);
+
+  // useEffect with an empty dependecy array [] to run only once
+  // This works like componentDidMount()
+  useEffect(() => {
+    // Fetching fake data from an online API
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then(response => response.json())
+      .then(json => setTableConent(json));
+  }, []);
 
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <table>
         <thead>
           <tr>
@@ -13,9 +26,9 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((content, index) => {
+          {tableContent.slice(0, infiniteScroll).map(content => {
             return (
-              <tr key={index}>
+              <tr key={content.id}>
                 <td style={{ paddingTop: "10px" }}>{content.userId}</td>
                 <td style={{ paddingTop: "10px" }}>{content.title}</td>
               </tr>
