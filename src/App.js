54244029b5eb5eb50f4d63e45914c132480a19d4
Craig Rice <craigricetong@gmail.com>
2020-02-23 10:57:29 -0800

    added todo data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from './data';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>User ID</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((content, index) => {
+            return (
+              <tr key={index}>
+                <td style={{ paddingTop: "10px" }}>{content.userId}</td>
+                <td style={{ paddingTop: "10px" }}>{content.title}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
